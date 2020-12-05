6a35e8b5ba1b3745f19a2268a5e70e46215267fb
rachfuerst <rach.fuerst@gmail.com>
2020-12-05 12:16:27 -0600

    Writing logic to make modal display only if a valid email is entered

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+//Get the Input box
+var input = document.getElementById("input");
+
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = function () {
+  if (input.value) {
+    input.value = "";
+    input.required = false;
+    modal.style.display = "block";
+   
+  } else {
+    modal.style.display = "none";
+  }
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
